3c341845d6053ab616d456f76de2d19b960bcd22
sahilmassive <sahil@gomassive.com>
2024-07-04 23:28:09 +0530

    swiper hero done

--- src/components/HeroSwiper.jsx
+++ src/components/HeroSwiper.jsx
@@ -5,7 +5,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Autoplay, Pagination } from "swiper/modules";
-import { Box } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 
 const HeroSwiper = () => {
   return (
@@ -34,7 +34,50 @@ const HeroSwiper = () => {
               marginTop: "95px",
               backgroundPosition: "center",
             }}
-          ></Box>
+          >
+            <Box
+              sx={{
+                backgroundImage: "url('/images/shadow.png')",
+                width: "100%",
+                height: "615px",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                marginTop: "95px",
+              }}
+            >
+              <Container>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    paddingTop: "115px",
+                    gap: "48px",
+                  }}
+                >
+                  <Typography
+                    color={"#E3FEF7"}
+                    fontSize={"clamp(2rem, 1.6887rem + 1.5094vw, 3.5rem)"}
+                    fontFamily={"myFirstFont"}
+                    lineHeight={"56px"}
+                  >
+                    Demolition
+                    <br /> Service
+                  </Typography>
+                  <Typography
+                    fontSize={"clamp(1.125rem, 1.0472rem + 0.3774vw, 1.5rem)"}
+                    color={"#fff"}
+                    fontFamily={"myThirdFont"}
+                  >
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting
+                    <br /> industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever
+                    <br /> since the 1500s,
+                  </Typography>
+                </Box>
+              </Container>
+            </Box>
+          </Box>
         </SwiperSlide>
         <SwiperSlide>
           <Box
@@ -47,7 +90,50 @@ const HeroSwiper = () => {
               marginTop: "95px",
               backgroundPosition: "center",
             }}
-          ></Box>
+          >
+            <Box
+              sx={{
+                backgroundImage: "url('/images/shadow.png')",
+                width: "100%",
+                height: "615px",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                marginTop: "95px",
+              }}
+            >
+              <Container>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    paddingTop: "115px",
+                    gap: "48px",
+                  }}
+                >
+                  <Typography
+                    color={"#E3FEF7"}
+                    fontSize={"clamp(2rem, 1.6887rem + 1.5094vw, 3.5rem)"}
+                    fontFamily={"myFirstFont"}
+                    lineHeight={"56px"}
+                  >
+                    Demolition
+                    <br /> Service
+                  </Typography>
+                  <Typography
+                    fontSize={"clamp(1.125rem, 1.0472rem + 0.3774vw, 1.5rem)"}
+                    color={"#fff"}
+                    fontFamily={"myThirdFont"}
+                  >
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting
+                    <br /> industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever
+                    <br /> since the 1500s,
+                  </Typography>
+                </Box>
+              </Container>
+            </Box>
+          </Box>
         </SwiperSlide>
         <SwiperSlide>
           <Box
@@ -60,7 +146,50 @@ const HeroSwiper = () => {
               marginTop: "95px",
               backgroundPosition: "center",
             }}
-          ></Box>
+          >
+            <Box
+              sx={{
+                backgroundImage: "url('/images/shadow.png')",
+                width: "100%",
+                height: "615px",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                marginTop: "95px",
+              }}
+            >
+              <Container>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    paddingTop: "115px",
+                    gap: "48px",
+                  }}
+                >
+                  <Typography
+                    color={"#E3FEF7"}
+                    fontSize={"clamp(2rem, 1.6887rem + 1.5094vw, 3.5rem)"}
+                    fontFamily={"myFirstFont"}
+                    lineHeight={"56px"}
+                  >
+                    Demolition
+                    <br /> Service
+                  </Typography>
+                  <Typography
+                    fontSize={"clamp(1.125rem, 1.0472rem + 0.3774vw, 1.5rem)"}
+                    color={"#fff"}
+                    fontFamily={"myThirdFont"}
+                  >
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting
+                    <br /> industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever
+                    <br /> since the 1500s,
+                  </Typography>
+                </Box>
+              </Container>
+            </Box>
+          </Box>
         </SwiperSlide>
       </Swiper>
     </Box>
