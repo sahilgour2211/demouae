e64a3553b28a1aa2574a6dc8a69978a6268eb16b
shivam30072 <shivam30072@gmail.com>
2024-09-08 14:10:54 +0530

    bug fixed

--- src/NewComponents/ContactUs.jsx
+++ src/NewComponents/ContactUs.jsx
@@ -14,6 +14,7 @@ import React, { useEffect, useState } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import emailjs from "@emailjs/browser";
 import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 const ContactUs = () => {
   const [formData, setFormData] = useState({
